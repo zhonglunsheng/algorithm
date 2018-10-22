48ad43619310495ece9bd5f7d2049ec1923363b0
zhonglunsheng <873946453@qq.com>
2018-10-22 23:36:42 +0800

    :construction:commit

--- 排序/QuickSort.java
+++ 排序/QuickSort.java
@@ -27,6 +27,7 @@ public class QuickSort {
 
     private static void quickSort(int[] arr, int l, int r) {
         if (l < r){
+            swap(arr,(int)Math.random() * (r - l + 1), r);
             int[] p = partition(arr, l, r);
             quickSort(arr, l, p[0] - 1);
             quickSort(arr, p[1] + 1, r);
