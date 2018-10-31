8a2871317684f1f05a847e4953f52096af498a63
zhonglunsheng <873946453@qq.com>
2018-10-31 22:04:33 +0800

    :construction:commit

---
+++ 链表/ReverseSinglelyList.java
@@ -0,0 +1,31 @@
+package com.lsheng.algorithm.linkedlist;
+/**
+ * @author zhonglunsheng
+ * @Description
+ * @create 2018-10-31 21:46
+ */
+public class ReverseSinglelyList {
+
+    public static void reverseSinglelyList(Node head){
+       Node pre = null;
+       Node next = null;
+
+       while (head != null){
+          next = head.next;
+          head.next = pre;
+          pre = head;
+          head = next;
+       }
+    }
+}
+
+
+class Node{
+    public int value;
+    public Node next;
+
+    public Node(int data){
+        this.value = data;
+    }
+
+}
