4b3b54258d649cbdd5965fb7539677b93fd4b458
zhonglunsheng <873946453@qq.com>
2019-01-04 05:09:57 +0800

    剑指offer 从上往下打印二叉树

---
+++ 二叉树/Solution.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            int ctn = queue.size();
+            
+            while(ctn-- > 0){
+                TreeNode node = queue.poll();
+                if(node == null){
+                    continue;
+                }
+                list.add(node.val);
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+        return list;
+    }
+}
